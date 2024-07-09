46402d4b0060689acf299ac8b7eb8c9564a9fa2f
DenisNysheta <nisetadenis@gmail.com>
2024-07-09 17:47:08 +0300

    Add project

---
+++ Weather/src/components/Box/Box.jsx
@@ -0,0 +1,17 @@
+// import styled from "styled-components"
+import "./Box.scss"
+
+export default function Box(props) {
+
+    // const BOX = styled.div`
+    //     width:500px;
+    //     height:500px;
+    //     background-color:${props.color == 2 ? "red" : props.color == 3 ? "green" : "blue"};
+    // `
+
+    return (
+        <>
+            <div className="box"></div>
+        </>
+    )
+}
